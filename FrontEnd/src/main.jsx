d89c8c63f681b69b67a4bfafdc3760c51b3dd420
jubaal <jubaal2122daw2@protonmail.com>
2022-05-06 15:50:14 +0200

    cambiadas las rutas de español a catalan. --> buscar api camara

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Idiomas from './routes/idiomas';
 import Espanyol from './routes/espanyol';
+import Catalan from './routes/catalan';
 import Abece from './routes/abece';
 
 //strict mode es para detectar fallos en ls hijos mientras vas desarrollando
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={<App />}/>
         <Route path='idiomas' element={<Idiomas />} />
         <Route path='espanyol' element={<Espanyol />} />
+        <Route path='catala' element={<Catalan />} />
         <Route path='abecedario' element={<Abece />} />
       </Routes>
     </BrowserRouter>
--- FrontEnd/src/routes/catalan.jsx
+++ FrontEnd/src/routes/catalan.jsx
@@ -6,19 +6,18 @@ import AbcIcon from '@mui/icons-material/Abc';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 
-export default function Espanyol() {
+export default function Catala() {
     return (
         <div>
             <div className="box-border h-18 w-32 p-4 ">
                 <Link className="back rounded-md" to='/idiomas'><ArrowBackIosNewIcon className="arrow" />Volver</Link>
             </div>
-            EN DESARROLLO
-            {/* <div className="container px-5 py-28 mx-auto grid content-center">
+            <div className="container px-5 py-28 mx-auto grid content-center">
                     <div className='flex flex-wrap flex-col m-auto gap-20'>
                         <Link className="rounded-full gap-10 ring-4 ring-teal-700 py-8 sub-menu" to='/abecedario'><AbcIcon style={{ fontSize: "8rem", color: "#de7a22" }} /></Link>
                         <Link className="rounded-full gap-10 ring-4 ring-teal-700 px-16 py-16 sub-menu" to='/abecedario'><CameraAltIcon style={{ fontSize: "3.5rem", color: "#de7a22"}} /></Link>
                     </div>
-            </div> */}
+            </div>
 
         </div>
     );
