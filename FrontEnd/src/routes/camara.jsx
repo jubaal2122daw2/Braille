da99b54b142fdf3f98b5478c962d982062fd31ca
jubaal <jubaal2122daw2@protonmail.com>
2022-05-18 18:33:18 +0200

    carga letra en otra screen y la camara en useeffect. --> poner diseño en screen letra

--- FrontEnd/src/routes/camara.jsx
+++ FrontEnd/src/routes/camara.jsx
@@ -3,52 +3,64 @@ import '../App.css';
 import { useEffect, useState } from 'react';
 import { Link, Outlet } from "react-router-dom";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import Letra from './letra';
 
 export default function Camara() {
     const URL = "./models/";
-    let model, webcam, labelContainer, maxPredictions;
+    let model, webcam, labelContainer, maxPredictions, tiempo;
+    let continuar = false;
     const [letra, setLetra] = useState('');
+    const [stopped, setStopped] = useState(false);
     console.log("La letra del hook -->", letra);
+    console.log(stopped);
 
-    // useEffect(() => {
-    //     init();
-    // }, [])
+    useEffect(() => {
+        if(!stopped) init();
+    }, [stopped]);
+
+    const handleChange = (newValue) => {
+        console.log("entra en handlechange");
+        setStopped(newValue);
+    }
+
+    function canceltiempo(){
+        clearTimeout(tiempo);
+    }
 
     // Load the image model and setup the webcam
-    let continuar = false;
 
     async function init() {
-            continuar = true;
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-            // const devices = await navigator.mediaDevices.enumerateDevices();
+        continuar = true;
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+        // const devices = await navigator.mediaDevices.enumerateDevices();
 
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // or files from your local hard drive
-            // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-            model = await tmImage.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // or files from your local hard drive
+        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
 
-            // Convenience function to setup a webcam
-            const flip = false; // whether to flip the webcam
-            webcam = new tmImage.Webcam(300, 300, flip); // width, height, flip
-            await webcam.setup({ facingMode: "environment" }); // request access to the webcam
-            await webcam.play();
-            window.requestAnimationFrame(loop);
+        // Convenience function to setup a webcam
+        const flip = false; // whether to flip the webcam
+        webcam = new tmImage.Webcam(300, 300, flip); // width, height, flip
+        await webcam.setup({ facingMode: "environment" }); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
 
-            let tiempo = setTimeout(() => {
-                webcam.stop();
-                continuar = false;
-            }, 9000);
+        // append elements to the DOM
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
 
-            // append elements to the DOM
-            document.getElementById("webcam-container").appendChild(webcam.canvas);
-            labelContainer = document.getElementById("label-container");
-            for (let i = 0; i < maxPredictions; i++) { // and class labels
-                labelContainer.appendChild(document.createElement("div"));
-            }
-        
+        tiempo = setTimeout(() => {
+            webcam.stop();
+            continuar = false;
+            setStopped(true);
+        }, 9000);
 
     }
 
@@ -67,20 +79,23 @@ export default function Camara() {
                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
             if (prediction[i].probability.toFixed(2) > 0.90) {
-                console.log(prediction[i].className);
                 setLetra(prediction[i].className);
             }
         }
     }
     return (
-        <div className='grid place-items-center h-screen bg-black'>
-            <div className="-mb-16 box-border h-18 w-32 p-4 place-self-start">
-                <Link className="back rounded-md" to='/catala'><ArrowBackIosNewIcon className="arrow" />Volver</Link>
-            </div>
-            <div className='text-teal-500' onClick={() => { init() }}>Click</div>
-            <div className='text-teal-500'>Centra el texto en la cámara para traducir</div>
-            <div id="webcam-container"></div>
-            <div id="label-container" className='text-teal-500'></div>
-        </div>
+        <>
+            {!stopped ? (
+                <div className='grid place-items-center h-screen bg-black'>
+                    <div className="-mb-16 box-border h-18 w-32 p-4 place-self-start">
+                        <Link onClick={() => {canceltiempo(), webcam.stop()} } className="back rounded-md" to='/catala'><ArrowBackIosNewIcon className="arrow" />Volver</Link>
+                    </div>
+                    {/* <div className='text-teal-500' onClick={() => { init() }}>Click</div> */}
+                    {/* <div className='text-teal-500'>Centra el texto en la cámara para traducir</div> */}
+                    <div id="webcam-container"></div>
+                    <div id="label-container" className='text-teal-500'></div>
+                </div>
+            ) : (<Letra letra={letra} cambiarStop={handleChange} />)}
+        </>
     );
 }
--- FrontEnd/src/routes/letra.jsx
+++ FrontEnd/src/routes/letra.jsx
@@ -4,8 +4,18 @@ import { useEffect, useState } from 'react';
 import { Link, Outlet } from "react-router-dom";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
-export default function Letra() {
-    <div className='grid place-items-center h-screen'>
-        
-    </div>
+export default function Letra(props) {
+
+    function handleChange(estado) {
+        props.cambiarStop(estado);
+    }
+
+    return (
+        <div className='grid place-items-center h-screen'>
+            <div className="-mb-16 box-border h-18 w-32 p-4 place-self-start">
+                <Link onClick={() => {handleChange(false)} } className="back rounded-md" to='/camara'><ArrowBackIosNewIcon className="arrow" />Volver</Link>
+            </div>
+            <h1>{props.letra}</h1>
+        </div>
+    );
 }
