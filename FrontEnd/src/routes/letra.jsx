882219985e65bc14ecee7ab6675794a67ed94699
jubaal <jubaal2122daw2@protonmail.com>
2022-05-20 05:01:19 +0200

    arreglado el css de las fichas. --> probar un ultimo modelo

--- FrontEnd/src/routes/letra.jsx
+++ FrontEnd/src/routes/letra.jsx
@@ -16,11 +16,11 @@ export default function Letra(props) {
             <div className="-mb-16 box-border h-18 w-32 p-4 place-self-start">
                 <Link onClick={() => { handleChange(false) }} className="back rounded-md" to='/camara'><ArrowBackIosNewIcon className="arrow" />Tornar</Link>
             </div>
-            <div className=''>
+            <div className='place-self-center'>
                 <p className='text-4xl mx-8 text-center'>La lletra escanejada és:</p>
             </div>
-            <div className='letra'>
-                <p className='text-center'>{props.letra}</p>
+            <div className='letra w-8/12 h-80 lg:h-min sm:w-4/5 lg:w-2/5 sm:h-min sm:mb-0 lg:mb-0 content-center self-center'>
+                <p className='text-center mt-6 sm:-mt-10 sm:py-6 lg:-mt-10' style={{fontSize: "8rem"}}>{props.letra}</p>
             </div>
 
             {/* <div><CampaignIcon className='megafono text-amber-700'></CampaignIcon></div> */}
