7422c1327a6135c8245953080d618e221b4f416f
jubaal <jubaal2122daw2@protonmail.com>
2022-05-22 05:17:14 +0200

    Ultimo commit del proyecto

--- FrontEnd/src/routes/letra.jsx
+++ FrontEnd/src/routes/letra.jsx
@@ -20,7 +20,7 @@ export default function Letra(props) {
                 <p className='text-4xl mx-8 text-center'>La lletra escanejada és:</p>
             </div>
             <div className='letra w-8/12 h-80 lg:h-min sm:w-4/5 lg:w-2/5 sm:h-min sm:mb-0 lg:mb-0 content-center self-center'>
-                <p className='text-center mt-6 sm:-mt-10 sm:py-6 lg:-mt-10' style={{fontSize: "8rem"}}>{props.letra}</p>
+                <p className='text-center mt-10 sm:-mt-10 sm:py-6 lg:-mt-10' style={{fontSize: "8rem"}}>{props.letra}</p>
             </div>
 
             {/* <div><CampaignIcon className='megafono text-amber-700'></CampaignIcon></div> */}
